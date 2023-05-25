ec7eeb67606ac60d1109cae1a673d4cb58ba2ff1
bbtit <the8butterfly8dream@gmail.com>
2023-05-25 23:50:00 +0900

    各シミュレーションを並列に実行するように変更

--- db-aco-base.py
+++ db-aco-base.py
@@ -3,6 +3,7 @@ import random
 import traceback
 import math
 import psycopg2
+from multiprocessing import Pool
 
 class Params:
     def __init__(self, num_nodes:int, optimal_route_length:int, volatility:float, pheromone_min:int, pheromone_max:int, ttl:int, bata:int, generation_limit:int, simulation_count:int) -> None:
@@ -307,19 +308,8 @@ class Simulation:
         return f'INSERT INTO simulations (ParameterID) VALUES ({self.params.id});'
     
     
-def main():
+def main(params:Params):
     try:
-        # パラメータを設定
-        params = Params(num_nodes=5, 
-                        optimal_route_length=2, 
-                        volatility=0.99, 
-                        pheromone_min=100, 
-                        pheromone_max=2**20, 
-                        ttl=100, 
-                        bata=1, 
-                        generation_limit=2, 
-                        simulation_count=1)
-
         # DBLoggerインスタンス作成
         dblogger = DBLogger("asaken_n40","asaken_N40","localhost","simulation","5432")
 
@@ -331,70 +321,66 @@ def main():
         if params.id is None:
             params.id:int = dblogger.fetch_result(params.generate_select_query())[0][0]
             print(params.id)
+
+        # Simulationインスタンス作成
+        simulation = Simulation(dblogger, params)
+
+        # シミュレーションを登録&シミュレーションIDを取得
+        simulation.id = dblogger.insert_and_get_id(simulation.generate_insert_query())
+
+        # 任意の個数ノードインスタンスを作成
+        simulation.network.yield_nodes(params)
+
+        # BAモデルになるようにノードを接続
+        simulation.network.make_ba_model(params, 3)
+
+        # 最適ルートを作成
+        simulation.network.make_optimal_route(params)
         
-        # シミュレーションをparams.simulation_count回実行
-        for _ in range(params.simulation_count):
-
-            # Simulationインスタンス作成
-            simulation = Simulation(dblogger, params)
-
-            # シミュレーションを登録&シミュレーションIDを取得
-            simulation.id = dblogger.insert_and_get_id(simulation.generate_insert_query())
-
-            # 任意の個数ノードインスタンスを作成
-            simulation.network.yield_nodes(params)
-
-            # BAモデルになるようにノードを接続
-            simulation.network.make_ba_model(params, 3)
-
-            # 最適ルートを作成
-            simulation.network.make_optimal_route(params)
-            
-            # ノードを登録&ノードIDを取得
-            for node in simulation.network.nodes:
-                node.id = dblogger.insert_and_get_id(node.generate_insert_query(simulation.id))
-            
-            # 任意の回数Generationを繰り返す
-            for generation_count in range(params.generation_limit):
-
-                # Genaerationを登録&GenerationIDを取得
-                generation_id = dblogger.insert_and_get_id(f'INSERT INTO generations (simulationid, generation_count) VALUES ({simulation.id},{generation_count});')
-
-                # Connectionsを登録
-                for startnode in simulation.network.nodes:
-                    for endnode, link in node.neighbors.items():
-                        dblogger.insert_and_get_id(f'INSERT INTO connections (GenerationID, StartNodeID, EndNodeID, Pheromone, Width) VALUES ({generation_id},{startnode.id},{endnode.id},{link.feromone},{link.width});')
-
-                # antとinterestの生成
-                simulation.ant = Ant(simulation.network.start_node, simulation.network.end_node)
-                simulation.interest = Interest(simulation.network.start_node, simulation.network.end_node)
-
-                # antの移動
-                simulation.ant.hop_if_movable(params)
-
-                # 目的地に到達していたらフェロモン付加
-                if simulation.ant.is_at_destination():
-                    simulation.network.add_pheromone_to_ant_route(simulation.ant)
-
-                # antの結果を登録
-                dblogger.execute_query(simulation.ant.get_insert_query(generation_id))
-
-                # antをNoneにして消去
-                simulation.ant = None
-
-                # interestの移動
-                simulation.interest.hop_if_movable(params)
-
-                # interestの結果を登録
-                dblogger.execute_query(simulation.interest.get_insert_query(generation_id))
-
-                # interestをNoneにして消去
-                simulation.interest = None
-
-                # フェロモン揮発
-                simulation.network.volitile_pheromone(params)
+        # ノードを登録&ノードIDを取得
+        for node in simulation.network.nodes:
+            node.id = dblogger.insert_and_get_id(node.generate_insert_query(simulation.id))
         
+        # 任意の回数Generationを繰り返す
+        for generation_count in range(params.generation_limit):
 
+            # Genaerationを登録&GenerationIDを取得
+            generation_id = dblogger.insert_and_get_id(f'INSERT INTO generations (simulationid, generation_count) VALUES ({simulation.id},{generation_count});')
+
+            # Connectionsを登録
+            for startnode in simulation.network.nodes:
+                for endnode, link in node.neighbors.items():
+                    dblogger.insert_and_get_id(f'INSERT INTO connections (GenerationID, StartNodeID, EndNodeID, Pheromone, Width) VALUES ({generation_id},{startnode.id},{endnode.id},{link.feromone},{link.width});')
+
+            # antとinterestの生成
+            simulation.ant = Ant(simulation.network.start_node, simulation.network.end_node)
+            simulation.interest = Interest(simulation.network.start_node, simulation.network.end_node)
+
+            # antの移動
+            simulation.ant.hop_if_movable(params)
+
+            # 目的地に到達していたらフェロモン付加
+            if simulation.ant.is_at_destination():
+                simulation.network.add_pheromone_to_ant_route(simulation.ant)
+
+            # antの結果を登録
+            dblogger.execute_query(simulation.ant.get_insert_query(generation_id))
+
+            # antをNoneにして消去
+            simulation.ant = None
+
+            # interestの移動
+            simulation.interest.hop_if_movable(params)
+
+            # interestの結果を登録
+            dblogger.execute_query(simulation.interest.get_insert_query(generation_id))
+
+            # interestをNoneにして消去
+            simulation.interest = None
+
+            # フェロモン揮発
+            simulation.network.volitile_pheromone(params)
+        
         dblogger.commit()
     
     except Exception as e:
@@ -407,4 +393,16 @@ def main():
         
 
 if __name__ == "__main__":
-    main()
+    # パラメータを設定
+    params = Params(num_nodes=5, 
+                    optimal_route_length=2, 
+                    volatility=0.99, 
+                    pheromone_min=100, 
+                    pheromone_max=2**20, 
+                    ttl=100, 
+                    bata=1, 
+                    generation_limit=2, 
+                    simulation_count=1)
+    
+    with Pool() as p:
+        p.map(main, [params] * params.simulation_count)
